35321ea83f7b8b9e8d53f4a5342f44799590ffe0
isaac <isaac@rubenfamily.com>
2020-12-31 00:51:01 -0800

    progress on hiding files on linux
    working on linux file hiding for windows - somthing is wrong with the syscall

--- hide.go
+++ hide.go
@@ -2,9 +2,26 @@
 
 package main
 
-import "log"
+import (
+	"os"
 
-func hideFile(file string, wg *WaitGroupBar) {
+	"golang.org/x/sys/unix"
+)
+
+//ATTR_HIDDEN is the hidden atribute for fat systems
+const ATTR_HIDDEN = 2
+
+//FAT_IOCTL_SET_ATTRIBUTES is write mode
+const FAT_IOCTL_SET_ATTRIBUTES = 0x80047211
+
+func hideFile(fileStr string, wg *WaitGroupBar) {
 	defer wg.Done()
-	log.Printf("hide method not implemented for this platform. %s cannot be hidden", file)
+
+	openFile, err := os.Open(fileStr)
+	handleErr(err)
+	defer openFile.Close()
+
+	handleErr(unix.IoctlSetInt(int(openFile.Fd()), FAT_IOCTL_SET_ATTRIBUTES, ATTR_HIDDEN))
+	//bunch of magic I dont really understand
+	//much thanks to chanbakjsd#7968 from the gophers discord
 }
