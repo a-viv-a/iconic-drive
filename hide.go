d06cb0881b0e4d0e0960375762ca4d192f7b79f1
isaac <isaac@rubenfamily.com>
2020-12-31 02:08:13 -0800

    getting closer
    this feels very close to working, but I may resort to just using cgo

--- hide.go
+++ hide.go
@@ -9,10 +9,13 @@ import (
 )
 
 //ATTR_HIDDEN is the hidden atribute for fat systems
-const ATTR_HIDDEN = 2
+const ATTR_HIDDEN int = 2
+
+//FAT_IOCTL_GET_ATTRIBUTES is read mode
+const FAT_IOCTL_GET_ATTRIBUTES uint = 0x80047210
 
 //FAT_IOCTL_SET_ATTRIBUTES is write mode
-const FAT_IOCTL_SET_ATTRIBUTES = 0x80047211
+const FAT_IOCTL_SET_ATTRIBUTES uint = 0x80047211
 
 func hideFile(fileStr string, wg *WaitGroupBar) {
 	defer wg.Done()
@@ -21,7 +24,12 @@ func hideFile(fileStr string, wg *WaitGroupBar) {
 	handleErr(err)
 	defer openFile.Close()
 
-	handleErr(unix.IoctlSetInt(int(openFile.Fd()), FAT_IOCTL_SET_ATTRIBUTES, ATTR_HIDDEN))
+	current, err := unix.IoctlGetInt(int(openFile.Fd()), FAT_IOCTL_GET_ATTRIBUTES)
+	handleErr(err)
+
+	//log.Printf("Current flags are: %d", current)
+
+	handleErr(unix.IoctlSetPointerInt(int(openFile.Fd()), FAT_IOCTL_SET_ATTRIBUTES, current|ATTR_HIDDEN))
 	//bunch of magic I dont really understand
-	//much thanks to chanbakjsd#7968 from the gophers discord
+	//much thanks to chanbakjsd#7968 and _diamondburned_#4507 from the gophers discord
 }
